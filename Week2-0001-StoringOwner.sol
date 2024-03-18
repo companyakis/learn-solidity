f221d7e5e4efd130fe82e3c9df9d117cd21e66e7
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-18 12:05:32 +0300

    Create Week2-0001-StoringOwner.sol

---
+++ Week2-0001-StoringOwner.sol
@@ -0,0 +1,33 @@
+/*
+Your Goal: Store the Owner
+Create a public address state variable called owner on the contract
+Next create a constructor function which will store the msg.sender in owner
+*/
+
+
+
+
+
+
+
+
+
+/*
+An address on the EVM is a 160 bits long, or a 40 character, hexadecimal string:
+
+address a = 0xc783df8a850f42e7f7e57013759c285caa701eb6;
+
+This is valid Solidity! We can store a fixed address in our contracts if we need to.
+
+We can also find the sender of the current message:
+
+import "forge-std/console.sol";
+contract Example {
+    constructor() {
+        console.log( msg.sender ); // 0xc783df8a850f42e7f7e57013759c285caa701eb6
+    }
+}
+
+ Here we are logging the address of the account calling this contract.
+
+*/
