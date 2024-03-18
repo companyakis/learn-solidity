50b0f092039e22eed767722df7b40a8d73030d6d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-18 12:05:53 +0300

    Update Week2-0001-StoringOwner.sol

--- Week2-0001-StoringOwner.sol
+++ Week2-0001-StoringOwner.sol
@@ -5,11 +5,18 @@ Next create a constructor function which will store the msg.sender in owner
 */
 
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract Contract {
 
+    address public owner;
 
-
-
+    constructor() {
+        owner = msg.sender;
+    }
+    
+}
 
 
 /*
